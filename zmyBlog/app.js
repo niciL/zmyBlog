94e5d2bff6dfb76d61469424add1a5d0ba002807
niciL <773892580@qq.com>
2017-03-03 21:05:28 +0800

    first commit

---
+++ zmyBlog/app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//设置app应用的路由架构，引入各个功能模块对应的导航模块
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+//解析post请求提交的数据
+app.use(bodyParser.json());//解析json数据
+app.use(bodyParser.urlencoded({ extended: false }));//解析普通数据，解析后的数据放到了req.body
+//获取cookie
+app.use(cookieParser());
+//设置静态路由
+app.use(express.static(path.join(__dirname, 'public')));
+
+//路由映射，路由的设定应该遵循Restful设计原则
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
